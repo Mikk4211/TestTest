64bb09a4b11a43ce9cbd4d37f80d3862e28164c6
Mikkel Sørensen <mik4211@edu.easj.dk>
2017-11-30 08:50:30 +0100

    Initial commit

---
+++ src/TestTest.java
@@ -0,0 +1,15 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import org.junit.Test;
+
+
+public class TestTest {
+
+    @Test
+  public void check() {
+      test checko = new test();
+      String sum = checko.overfør(250);
+      assertEquals(checko.ikkelort, sum);
+    }
+
+}
---
+++ src/test.java
@@ -0,0 +1,20 @@
+public class test {
+
+ int kontoNuv = 1000;
+ int overførsel = 0;
+
+ String lort = "fuck u";
+ String ikkelort = "gj no fuckup";
+
+ public String overfør(int overførsel){
+
+     if (overførsel>kontoNuv) {
+     return lort;
+     }
+     else {return ikkelort;}
+
+ }
+
+ }
+
+
